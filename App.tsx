ffdd51fa149ce7ce4609f618906f06ffce1cb3aa
rumeysazeynep <rumeysazeynepkabadayi@hotmail.com>
2026-01-09 13:51:29 +0300

    Bottom tabs designed and navigation functionality provided using React Navigation.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
 import "./global.css";
-import { StyleSheet, Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white" style={styles.container}>
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+      <AppNavigator />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ProductScreen from '../screens/ProductScreen';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Dimensions, StyleSheet } from 'react-native';
+import FavScreen from '../screens/FavScreen';
+import { View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
+const TAB_BAR_HEIGHT = Math.round(SCREEN_HEIGHT * 0.085);
+const TAB_BAR_RADIUS = Math.round(TAB_BAR_HEIGHT / 2);
+
+type EntypoName = React.ComponentProps<typeof Entypo>['name'];
+
+function HomeTabs(){
+
+  const insets = useSafeAreaInsets();
+  return(
+    <SafeAreaView style={styles.safeArea}>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle:[
+              styles.tabBar,
+              {
+                bottom: insets.bottom + 2,
+              }
+          ],
+          tabBarItemStyle: styles.tabBarItem,
+          tabBarIcon: ({focused}) => renderTabIcon(route.name, focused)
+        })}>
+        <Tab.Screen name="home" component={HomeScreen} />
+        <Tab.Screen name="fav" component={FavScreen} />
+        <Tab.Screen name="product" component={ProductScreen} />
+        
+      </Tab.Navigator>
+    </SafeAreaView>
+  )
+}
+
+const renderTabIcon = (routeName:string, focused:boolean) => {
+    let iconName:EntypoName = 'home';
+    if(routeName === "home"){
+      iconName = 'home';
+    }else if(routeName === "fav"){
+      iconName = 'heart';
+    }else if(routeName === "product"){
+      iconName = 'shopping-cart';
+    }
+
+    const containerClass = `
+      w-12 
+      h-12 
+      rounded-full 
+      items-center
+      justify-center
+      ${focused ? 'bg-gray-200' : ''}  
+    `
+
+    return(
+      <View className={containerClass.trim()}>
+          <Entypo name={iconName} size={24} color= {focused ? '#d4a574' : '#fff'}/>
+      </View>
+    )
+
+
+} 
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          contentStyle:{backgroundColor:"#000"}
+        }}>
+        <Stack.Screen name="Home" component={HomeTabs} options={{headerShown: false}} />
+        <Stack.Screen name="Product" component={ProductScreen} options={{headerShown: false}}  />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  tabBar: {
+    position: 'absolute',
+    backgroundColor: '#d4a574',
+    marginRight: SCREEN_WIDTH * 0.05,
+    marginLeft: SCREEN_WIDTH * 0.05,
+    height: TAB_BAR_HEIGHT,
+    width: SCREEN_WIDTH * 0.9,
+    borderRadius: TAB_BAR_RADIUS,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation:5
+  },
+  tabBarItem: {
+    marginTop: 8,
+  }
+});
